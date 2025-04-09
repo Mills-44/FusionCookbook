5f2a02d8542737941c1f4da12f3133cc64c542ec
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-09 00:16:49 -0700

    Update mills_cookbook.lua

--- mills_cookbook.lua
+++ mills_cookbook.lua
@@ -1,11 +1,20 @@
+
 -- ATLAS
+
+--Pepper Atlas
 SMODS.Atlas {
     key = "pepper_jokers", --For the pepper jokers implemented in mod
     path = "pepper_jokers.png", --image used for peppers
     px = 71, --Sizing of jokers in 1x
     py = 95 --1x y direction
 }
-
+-- Sweets Atlas
+SMODS.Atlas {
+    key = "sweets_jokers", --For the pepper jokers implemented in mod
+    path = "sweets_jokers.png", --image used for peppers
+    px = 71, --Sizing of jokers in 1x
+    py = 95 --1x y direction
+}
 -- Load Pepper Jokers
 local pepper_jokers = {
 "pimento_pepper", --Lvl 1
@@ -16,10 +25,22 @@ local pepper_jokers = {
 "ghost_pepper", --Lvl 6
 "carolina_reaper" --Final Lvl
 }
-
 for _, joker in ipairs(pepper_jokers) do
     local path = SMODS.current_mod.path .. "/jokers/peppers/" .. joker .. ".lua"
     local joker_def = dofile(SMODS.current_mod.path .. "/jokers/peppers/" .. joker .. ".lua")
 SMODS.Joker(joker_def)
+end
 
+-- Load Sweets Jokers
+local sweets_jokers = {
+    "wonkas_chocolate_bar",
+    "nutty_buddy",
+    "jammin_jelly",
+    "stay_puft",
+    "Barry_b",
+}
+for _, joker in ipairs(sweets_jokers) do
+    local path = SMODS.current_mod.path .. "/jokers/sweets/" .. joker .. ".lua"
+    local joker_def = dofile(SMODS.current_mod.path .. "/jokers/sweets/" .. joker .. ".lua")
+SMODS.Joker(joker_def)
 end

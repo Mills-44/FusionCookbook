a474710e485094524f74838b78a5cfa59e9c51cc
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-09 23:08:23 -0700

    Update ghost_pepper.lua

--- jokers/peppers/ghost_pepper.lua
+++ jokers/peppers/ghost_pepper.lua
@@ -11,95 +11,46 @@ return {
     },
     config = {
         extra = {
-            gain = 100,
-            mult = 0
+            mult = 100,
+            Xmult = 2
         }, 
         levels = 0
     }, 
     atlas = 'pepper_jokers',
     pos = {x = 4, y = 0 },
     unlocked = false,
-    discovered = true,
+    discovered = false,
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
     loc_vars = function(self, info_queue, card)
+        local total = card.ability.extra.mult + (G.GAME and G.GAME.hands and G.GAME.hands.mult or 1) * (card.ability.extra.Xmult - 1)
         return { 
-            vars = { 
-                card.ability.extra.gain or 100,
-                card.ability.extra.mult or 0
+            vars = {
+                card.ability.extra.mult or 100,
+                math.floor(total)
             }
         }
     end,
     calculate = function(self, card, context)
         if context.joker_main then
             return {
-                mult = card.ability.extra.mult,
-                message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } },
-                card = card
+                message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } },
+                Xmult_mod = card.ability.extra.Xmult,
+                mult_mod = card.ability.extra.mult
             }
-        end
-    
-        if context.before and (context.poker_hands["Flush"] or context.poker_hands["Full House"]) then
+        end   
+        if context.before and context.scoring_name and (context.scoring_name == "Flush" or context.scoring_name == "Full House") then
             card.ability.levels = (card.ability.levels or 0) + 1
-            local base_mult = (G.GAME.round_reserve and G.GAME.round_reserve.mult) or 0
-            local final_mult = (base_mult * 2) + 100
-            G.E_MANAGER:add_event(Event({
-                func = function()
-                    if not G.GAME.round_reserve then
-                        G.GAME.round_reserve = { mult = 0 }
-                    end
-                    ease_variable(G.GAME.round_reserve, 'mult', base_mult, final_mult, 0.5, 'easeOutCubic')
-                    G.GAME.round_reserve.mult = final_mult
-                    return true
-                end
-            }))
-            local result = {
-                message = 'Scorching!',
-                colour = G.C.XMULT,
-                card = card
-            }
-    
-            if card.ability.levels >= 2 then
-                card.ability.levels = 0
-               --Flags new version
-               card.ability.evolve_to = "j_mills_carolina_reaper"
-            end
-                return result
-        end
-            -- Loop used for creating new card and to delete previous joker 
-        if context.after and card.ability.evolve_to then
-        local evolve_key = card.ability.evolve_to
-        card.ability.evolve_to = nil
-        SMODS.add_card({key = evolve_key})
-            local cards= G.jokers.cards
-            local new_card = cards[#cards]
-            if new_card then  
-                new_card.ability.extra.mult = card.ability.extra.mult or 0
-                new_card.ability.levels = 0
-            end 
             G.E_MANAGER:add_event(Event({
                 func = function()
                     play_sound('tarot1')
                     card.T.r = -0.2
                     card:juice_up(0.3, 0.4)
-                    card.states.drag.is = true
-                    card.children.center.pinch.x = true
-            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-                func = function()
-                    G.jokers:remove_card(card) --Previous Joker deletes
-                    card:remove()
-                    card = nil
-                    return true; 
+                    return true
                 end
-            })) 
-            return true
+            }))
+            end
         end
-        })) 
-        return {
-            message = "Ciao!",--Message displayed as joker leaves
-            colour = G.C.CHIPS --Blue Text
-            }     
-        end
-    end
-}
+            }
+    

4529be8275a4a7fca6897c0a1560127684221690
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-12 08:06:17 +0000

    adding new snack items

---
+++ items/snack_cards.lua
@@ -0,0 +1,112 @@
+
+-- To Combine and Make Sandwhich Jokers
+local sandwich_recipes = {
+    ['wonkas_chocolate_bar+barry_b'] = 'everlasting_pollenstopper',
+    ['nutty_buddy+jammin_jelly'] = 'mrpb_and_jelly',
+    ['nutty_buddy+wonkas_chocolate_bar'] = 'golden_crunch',
+    ['stay_puft+barry_b'] = 'flayfluff_sub',
+    ['stay_puft+jammin_jelly'] = 'goopwhich'
+}
+
+local function get_recipe_key(j1, j2)
+    local key1, key2 = j1.config.center.key, j2.config.center.key
+    local recipe_key = sandwich_recipes[key1 .. '+' .. key2] or sandwich_recipes[key2 .. '+' .. key1]
+    return recipe_key
+end
+
+-- Bread card to make sandwhiches
+local slice_of_bread = {
+    key = 'slice_of_bread',
+    loc_txt = {
+        name = 'Slice Of Bread',
+        text = { 
+            "Combine {C:attention}2 compatible Jokers{} into",
+            "a {C:attention}Sandwich Joker{}"
+        }
+    },
+    config = {},
+    atlas = 'snack_cards',
+    pos = {
+        x = 0,
+        y = 0
+    },
+    discovered = true,
+    unlocked = true,
+    booster_pack = 'snack_packs',
+    use = function(self, card)
+        G.E_MANAGER:add_event(Event({
+            blocking = true,
+            func = function()
+                local selected = G.SELECTED_CARDS
+                if #selected == 2 and selected[1].area == G.jokers and selected[2].area == G.jokers then
+                    local j1, j2 = selected[1], selected[2]
+                    if j1.ability.sandwichable and j2.ability.sandwichable then
+                        local new_key = get_recipe_key(j1, j2)
+                        if new_key then
+                            local new_joker = create_card('Joker', G.jokers, new_key, 1, nil, nil)
+                            j1:remove()
+                            j2:remove()
+                            play_sound('tasty1')
+                            G.hand_text:add({text = 'Scrumptious!', scale = 1.2, color = G.C.MONEY})
+                        else
+                            G.hand_text:add({text = 'That Ain\'t a Sandwhich!', scale = 1, color = G.C.RED})
+                        end
+                    else
+                        G.hand_text:add({text = 'Incompatible Jokers!', scale = 1, color = G.C.RED})
+                    end
+                else
+                    G.hand_text:add({text = 'Select 2 Jokers', scale = 1, color = G.C.RED})
+                end
+                return true
+            end
+        }))
+    end
+}
+
+local cream_puff = {
+    key = 'cream_puff',
+    loc_txt = {
+        name = "Cream Puff",
+        text = {
+            "Boosts all {C:attention}Sweet Jokers{} by {C:mult}+20%{} stats",
+            "{C:inactive}(Chips, Mult, XMult, etc)"
+        }
+    },
+    config = {},
+    atlas = 'snack_cards',
+    pos = {
+        x = 0,
+        y = 0
+    },
+    discovered = true, 
+    unlocked = true,
+    booster_pack = 'snack_pack',
+    use = function(self, card)
+        G.E_MANAGER:add_event(Event({
+            blocking = true,
+            func = function()
+                local count = 0
+                for _, j in ipairs(G.jokers.cards) do
+                    if j.ability and j.ability.tags and j.ability.tags.sweet then
+                        j.ability.perma_bonus = j.ability.perma_bonus or {}
+                        local bonus = j.ability.perma_bonus
+                        bonus.mult = math.floor((bonus.mult or 0) * 1.2 + 0.5)
+                        bonus.x_mult = math.floor((bonus.x_mult or 0) * 1.2 + 0.5)
+                        bonus.chips = math.floor((bonus.chips or 0) * 1.2 + 0.5)
+                        j:juice_up(0.5)
+                        count = count + 1
+                    end
+                end
+                if count > 0 then
+                    play_sound('tasty1')
+                    G.hand_text:add({text = 'Sweet Boost!', scale = 1.2, color = G.C.MULT})
+                else
+                    G.hand_text:add({text = 'No Sweet Jokers!', scale = 1.1, color = G.C.RED})
+                end
+                return true
+            end
+        }))
+    end
+}
+
+return {slice_of_bread, cream_puff}
---
+++ items/snack_pack.lua
@@ -0,0 +1,25 @@
+-- Snack Pack booster 
+return {
+    key = 'snack_pack',
+    loc_txt = {
+        name = 'Snack Pack',
+        text = {
+             "Choose {C:attention}1{} of {C:attention}4{} Snack items",
+            "to {C:attention}munch{} on"
+        }
+    },
+   cost = 3,
+   discovered = true,
+   unlocked = true,
+   blueprint_compat = false,
+   booster_pack = true,
+   consumable_choices = {
+    'slice_of_bread',
+    'cream puff'
+},
+    no_general_pool = true,
+    atlas = 'snack_pack',
+    pos = {
+        x = 0, 
+        y = 0}
+}

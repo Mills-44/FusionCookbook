76d630f571edaa3b336204e99020fb9cd1dbfeb5
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-12 08:08:11 +0000

    new sweet jokers

---
+++ jokers/sweets/gummibar.lua
@@ -0,0 +1,71 @@
+return {
+    key = 'gummibar',
+    loc_txt = {
+        name = 'Gummibar',
+        text = {
+           "When scoring hand contains a {C:attention}Two Pair,",
+           "this {C:joker}Joker{} gains +3 {C:chips}Mult{}"
+        }
+    },
+    config = {
+        extra = {
+            gain = 3,
+            mult = 0
+        }, 
+    }, 
+    atlas = 'sweets_jokers',
+    pos = {
+        x = 0, 
+        y = 0 
+    },
+    cost = 5,
+    rarity = 1,
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    loc_vars = function(self, info_queue, card)
+        return { 
+            vars = { 
+                card.ability.extra.gain or 3,
+                card.ability.extra.mult or 0
+            }
+        }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main and not context.before then
+        return {
+            chips = card.ability.extra.mult
+          }
+        end
+        if context.joker_main and not context.before then
+            return {
+                mult = card.ability.extra.mult
+            }
+        end
+        if context.before and context.full_hand then
+            local pair_count = 0
+            local rank_counts = {}
+            for _, c in ipairs(context.full_hand) do
+                local v = c.base.value
+                rank_counts[v] = (rank_counts[v] or 0) + 1
+            end
+
+            for _, count in pairs(rank_counts) do
+                if count == 2 then
+                    pair_count = pair_count + 1
+                end
+            end
+
+            if pair_count == 2 then
+                card.ability.extra.mult = (card.ability.extra.mult or 0) + card.ability.extra.gain
+                return {
+                    message = "Beba bi duba duba yum yum!",
+                    colour = G.C.MULT,
+                    card = card
+                }
+            end
+        end
+    end
+}
---
+++ jokers/sweets/peppermint_butler.lua
@@ -0,0 +1,69 @@
+return {
+    key = 'peppermint_butler',
+    loc_txt = {
+        name = 'Peppermint Butler',
+        text = {
+           "When scoring hand contains a {C:attention}Pair{},",
+           "this {C:joker}Joker{} gains +5 {C:chips}Chips{}"
+        }
+    },
+    config = {
+        extra = {
+            gain = 5,
+            chips = 0
+        }, 
+    }, 
+    atlas = 'sweets_jokers',
+    pos = {
+        x = 0, 
+        y = 0 
+    },
+    cost = 4,
+    rarity = 1,
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    loc_vars = function(self, info_queue, card)
+        return { 
+            vars = { 
+                card.ability.extra.gain or 5,
+                card.ability.extra.chips or 0
+            }
+        }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main and not context.before then
+        return {
+            chips = card.ability.extra.chips
+          }
+        end
+        if context.before and context.scoring_name and (context.scoring_name == "") then
+            if context.before and context.full_hand then
+                local has_pair = false
+                local rank_counts = {}
+            
+                for _, c in ipairs(context.full_hand) do
+                    rank_counts[c.base.value] = (rank_counts[c.base.value] or 0) + 1
+                end
+            
+                for _, count in pairs(rank_counts) do
+                    if count == 2 then
+                        has_pair = true
+                        break
+                    end
+                end
+            
+                if has_pair then
+                    card.ability.extra.chips = (card.ability.extra.chips or 0) + card.ability.extra.gain
+                return {
+                    message = "Minty!",
+                    colour = G.C.CHIPS,
+                    card = card
+                }
+                end
+            end
+        end
+    end
+        }

95b907029d12363667e6fba11eed1d08b2efcfc4
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-28 23:04:51 -0700

    Update utility.lua

--- lib/utility.lua
+++ lib/utility.lua
@@ -8,3 +8,21 @@ function safe_random_choice(tbl, fallback)
     end
     return tbl[math.random(#tbl)]
 end
+
+function register_joker_to_pool(pool_key, joker_key)
+    local full_key = "j_mills_" .. joker_key
+
+    -- Create ObjectType if missing
+    if not SMODS.ObjectTypes[pool_key] then
+        SMODS.ObjectType{
+            object_type = "ObjectType",
+            key = pool_key,
+            default = full_key,
+            cards = {},
+            rarities = {}
+        }
+    end
+
+    -- Add joker to the pool
+    SMODS.ObjectTypes[pool_key].cards[full_key] = true
+end

95fd3d18c5e298bcc500c8dd9bd34510ca69034d
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-30 16:43:15 -0700

    Add files via upload

---
+++ items/snack/homeys_doney.lua
@@ -0,0 +1,30 @@
+SMODS.Consumable  {
+    key = 'homeys_doney',
+    set = 'Snack',
+    atlas = 'homeys_doney',
+    pos = { x = 0, y = 0 },
+    config = {},
+    cost = 3,
+    unlocked = true,
+    discovered = true,
+  
+    can_use = function(self)
+      return true
+    end,
+  
+    use = function(self, card, area)
+      local add_hand = MILLS.random_chance(0.5)
+      G.E_MANAGER:add_event(Event({
+        trigger = 'before', delay = 0,
+        func = function()
+          if add_hand then
+            ease_hands_played(1)
+          else
+            ease_discard(1)
+          end
+          play_sound('mills_doh')
+          return true
+        end
+      }))
+    end
+  }

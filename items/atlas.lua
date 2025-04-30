21567da216d73d396bdcc15ca558b4588968516d
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-30 02:59:07 +0000

    new game base

---
+++ items/atlas.lua
@@ -0,0 +1,47 @@
+--I know different from most formats but I enjoy redoing my art sicne I am pob gonna update decently often once I get better
+--If you are intrested in aty feel free to reach out :)
+
+local atlas_keys = { --This will set up a local loop of all atlas keys
+  'pepper_jokers',
+  'apple_of_jacks_eye',
+  'barry_b',
+  'gummibar',
+  'honey_nut_cheerios',
+  'jammin_jelly',
+  'koolaid_man',
+  'nutty_buddy',
+  'peppermint_butler',
+  'stay_puft',
+  'wonkas_chocolate_bar',
+  'everlasting_pollenstopper',
+  'flayfluff_sub',
+  'gloopwich',
+  'golden_crunch',
+  'mr_pb_and_jelly',
+  'snack_pack',
+  'snack_pack_jumbo',
+  'snack_pack_mega',
+  'munch',
+  'biscoff',
+  'bubblegum',
+  'butterscotch',
+  'cream_puff',
+  'homeys_doney',
+  'kinder_egg',
+  'pretzel',
+  'rye_chip',
+  'slice_of_bread',
+  'candisimo',
+  'cookiesimo',
+  'cookie',
+  --
+}
+
+for _, key in ipairs(atlas_keys) do --Pulls all keys in list
+  SMODS.Atlas {
+    key  = key, -- pulls the key name like 'pretzel'
+    path = key .. '.png', -- Adds .png to it for it to know its an image ofc for 'pretzel.png'
+    px   = 71, -- Size of horizontal direction for game cards in 1x
+    py   = 95, -- Size of vertical direction for game cards in 1x
+  }
+end
---
+++ items/sounds.lua
@@ -0,0 +1,21 @@
+-- Same as atlas just took the idea for it highly suggest doing same unless you want certain pitches for sounds
+-- Just set this loop up for any sound that you want that to have more explained in loop
+
+local sound_files = { -- loop of all soinds implemented in game
+    "adios", "bye", "crunch", "death", "doh", "fiery",
+    "goopy", "gummy", "honey", "jam", "jazz", "koolaid",
+    "later", "minty", "nutty", "piquant", "puft",
+    "sayonara", "scorching", "sizzling", "spicy", "tangy", "wonka",
+    "jack", "cookie", "bob", "gingy", "candy", 
+}
+
+for _, key in ipairs(sound_files) do -- Pulls all sounds in local loop
+    SMODS.Sound{
+        key  = key, -- Pulls key of sound
+        path = key .. '.ogg', -- Format Balatro uses and what I would do --> "bye.ogg" 
+        volume = 1.0, -- Volume of Sound
+        pitch = 1.0 -- Pitch Of Sound
+    }
+end
+
+--Copy this loop and just change volume/pitch if you want different settings for certain sounds :)

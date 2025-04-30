822d7172162bbaabcb8b7d81c6a5dd117aa0f0e9
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-30 16:41:54 -0700

    Add files via upload

---
+++ items/snack_pack.lua
@@ -0,0 +1,18 @@
+SMODS.Booster {
+    key = 'snack_pack',
+    group_key = 'mills_snack_pack',
+    atlas = 'snack_pack',
+    pos = { 
+        x = 0, 
+        y = 0 
+    },
+    config = {
+      extra = 4,
+      choose = 1
+    },
+    weight = 1,
+    cost = 4,
+    discovered = true,
+    draw_hand = true
+  }
+  

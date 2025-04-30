17cad003072e99d431f415ca2d072a40c5f148ca
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-30 16:42:37 -0700

    Add files via upload

---
+++ items/snack/boosters/snack_pack_jumbo.lua
@@ -0,0 +1,18 @@
+SMODS.Booster {
+    key = 'snack_pack_jumbo',
+    group_key = 'mills_snack_pack',
+    atlas = 'snack_pack_jumbo',
+    pos = { 
+        x = 0, 
+        y = 0 
+    },
+    config = {
+      extra = 5,
+      choose = 1
+    },
+    weight = .75,
+    cost = 6,
+    discovered = true,
+    draw_hand = true
+  }
+  

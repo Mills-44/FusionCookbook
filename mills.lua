3090d51e5219795146a871aaede3ad9af853100d
Mills-44 <mylesbrodsky44@gmail.com>
2025-04-30 02:54:53 +0000

    Updated Main

--- mills.lua
+++ mills.lua
@@ -1,114 +1,70 @@
+--For my Util and Define
 if not MILLS then
-	MILLS = {}
+  MILLS = {}
+end
+--Loads Mod
+mills = SMODS.current_mod
+mills_path = SMODS.current_mod.path
+
+--Config Lib
+MILLS.config = SMODS.load_file("config.lua")() -- When I set up UI I this will actually do sum for now its a dummy config 
+
+--Load lib
+SMODS.load_file("lib/define.lua")() -- Definitions 
+SMODS.load_file("lib/util.lua")() -- Utility functions built
+--SMODS.load_file("lib/ui.lua")() -- Not Today :(
+
+SMODS.load_file("items/atlas.lua")() -- Pictures
+SMODS.load_file("items/sounds.lua")() -- Sounds
+
+MILLS.register_items(MILLS.PEPPER_JOKERS, "items/joker/pepper")
+--MILLS.register_items(MILLS.SWEET_JOKERS, "items/joker/sweet")
+--MILLS.register_items(MILLS.SANDWICH_JOKERS, "items/joker/sandwich")
+--MILLS.register_items(MILLS.SNACKBOOSTER, "items/snack/boosters")
+--MILLS.register_items(MILLS.SNACKS, "items/snack/snacks")
+--MILLS.register_items(MILLS.ENHANCEMENTS, "items/enhancements")
+--MILLS.register_items(MILLS.SPECTRAL, "items/spectral")
+--MILLS.register_items(MILLS.SEALS, "items/seals")
+--MILLS.register_items(MILLS.TAGS, "items/tags")
+--MILLS.register_items(MILLS.EDITIONS, "items/editions")
+
+
+
+-- Collect valid objects to apply Cookbook config to
+local objects = {}
+
+for _, v in pairs(SMODS.Centers) do
+objects[#objects + 1] = { obj = v, center = true }
 end
 
-mills = SMODS.current_mod
+for _, v in pairs(SMODS.Tags) do
+objects[#objects + 1] = { obj = v, tag = true }
+end
 
-local atlas_keys = {
-    'pepper_jokers',
-    'apple_of_jacks_eye',
-    'barry_b',
-    'gummibar',
-    'honey_nut_cheerios',
-    'jammin_jelly',
-    'koolaid_man',
-    'nutty_buddy',
-    'peppermint_butler',
-    'stay_puft',
-    'wonkas_chocolate_bar',
-    'everlasting_pollenstopper',
-    'flayfluff_sub',
-    'gloopwich',
-    'golden_crunch',
-    'mr_pb_and_jelly',
-    'snack_pack',
-    'snack_pack_jumbo',
-    'snack_pack_mega',
-    'munch',
-    'biscoff',
-    'bubblegum',
-    'butterscotch',
-    'cream_puff',
-    'homeys_doney',
-    'kinder_egg',
-    'pretzel',
-    'rye_chip',
-    'slice_of_bread',
-    'candisimo',
-    'cookiesimo',
-    'cookie',
-    -- 
-  }
-  
-for _, key in ipairs(atlas_keys) do
-    SMODS.Atlas {
-      key  = key,
-      path = key .. '.png',
-      px   = 71,
-      py   = 95,
-    }
-  end
-  
--- ||       SOUNDS         ||
-local sound_keys = {
-    'tangy',
-    'sayonara',
-    'piquant',
-    'bye',
-    'spicy',
-    'adios',
-    'sizziling',
-    'fiery',
-    'later',
-    'death',
-    'scorching',
-    'crunch',
-    'goopy',
-    'doh',
-    'munch',
-    'wonka',
-    'jack',
-    'honey',
-    'nutty',
-    'puft',
-    'minty',
-    'gummy',
-    'jam',
-    'koolaid'
-}
-for _, key in ipairs(sound_keys) do
-    SMODS.Sound{
-      key    = key,
-      path   = key .. '.ogg',
-      volume = 1.0,
-      pitch  = 1.0,
-    }
+-- Apply said config to each valid object
+for _, v in ipairs(objects) do
+local obj = v.obj
+if obj and type(obj) == "table" and obj.mills then
+  local func_ref = obj.in_pool or function() return true end
+  local config = obj.mills
+
+  config.requirements = {}
+  for k, _ in pairs(config) do
+    local data = MILLS.requirement_map[k]
+    if data then
+      table.insert(config.requirements, data)
+    end
   end
 
---assert(SMODS.load_file("lib/utility.lua"))()
+-- Hook the in_pool function, adding extra logic depending on the
+-- config provided by this center
+  obj.in_pool = function(self, args)
+    local ret, dupes = func_ref(self, args)
 
-assert(SMODS.load_file("items/snack.lua"))()
-assert(SMODS.load_file("items/jokers/peppers.lua"))()
-assert(SMODS.load_file("items/jokers/sweet.lua"))()
-assert(SMODS.load_file("items/jokers/sandwiches.lua"))()
-assert(SMODS.load_file("items/modifiers.lua"))()
-assert(SMODS.load_file("items/spectrals.lua"))()
-assert(SMODS.load_file("items/seals.lua"))()
-
-   -- Grab the original
-   local orig_init = Sprite.init
-
-   function Sprite:init(x, y, w, h, atlas, pos, ...)
-       if not atlas then
-           local fallback_key, fallback_atlas
-           for k,a in pairs(G.ASSET_ATLAS) do
-               fallback_key, fallback_atlas = k, a
-               break
-           end
-           atlas = fallback_atlas
-           pos   = pos or { x = 0, y = 0 }
-       end
-
-       -- now call the original with a non‐nil atlas
-       orig_init(self, x, y, w, h, atlas, pos, ...)
-   end
+    for _, req in ipairs(config.requirements) do
+      ret = ret and MILLS.config[req.setting]
+    end
+    return ret, dupes
+  end
+end
+end

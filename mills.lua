163e0a8eff4617ecfa36271ac4712fc784dc1e04
Mills-44 <mylesbrodsky44@gmail.com>
2025-05-02 15:08:20 -0700

    Update mills.lua

--- mills.lua
+++ mills.lua
@@ -19,6 +19,7 @@ SMODS.load_file("items/sounds.lua")() -- Sounds
 
 MILLS.register_items(MILLS.PEPPER_JOKERS, "items/joker/pepper")
 MILLS.register_items(MILLS.SWEET_JOKERS, "items/joker/sweet")
+MILLS.register_items(MILLS.FUSION_JOKERS, "items/joker/fusion")
 --MILLS.register_items(MILLS.SANDWICH_JOKERS, "items/joker/sandwich")
 
 -- Register custom ConsumableType: Snack
@@ -34,9 +35,24 @@ SMODS.ConsumableType {
       name = 'Snack',
       label = 'Snack',
       },
-  shop_rate = 1.0,    
+  shop_rate = 1.0,
 }
 
+-- Register custom ConsumableType: Fusion
+SMODS.ConsumableType {
+  object_type = "ConsumableType",
+  key = 'Fusion',
+  default = 'c_mills_global',
+  collection_rows = { 4,4 },
+  primary_colour = HEX("7164C6"),
+  secondary_colour = HEX("31353B"),
+  loc_txt = {
+      collection = 'Fusion Cards',
+      name = 'Fusion',
+      label = 'Fusion',
+      },
+  shop_rate = 0.0,
+    }
 
 -- PATCH: Ensure pool is initialized before injection
 local orig_insert_pool = SMODS.insert_pool
@@ -47,14 +63,16 @@ SMODS.insert_pool = function(pool, center, ...)
   return orig_insert_pool(pool, center, ...)
 end
 
-MILLS.register_items(MILLS.SNACKBOOSTER, "items/snack/boosters")
-MILLS.register_items(MILLS.SNACKS, "items/snack/snacks")
-
+MILLS.register_items(MILLS.BOOSTER, "items/boosters")
 
 MILLS.register_items(MILLS.SWEET_ENHANCEMENTS, "items/enhancements/sweet")
 MILLS.register_items(MILLS.ORE_ENHANCEMENTS, "items/enhancements/ore")
 
-MILLS.register_items(MILLS.SPECTRAL, "items/spectral")
+MILLS.register_items(MILLS.SNACKS, "items/snack")
+--MILLS.register_items(MILLS.FUSION, "items/fusion")
+
+--MILLS.register_items(MILLS.SPECTRAL, "items/spectral")
+--MILLS.register_items(MILLS.TAROT, "items/tarot")
 
 MILLS.register_items(MILLS.SEALS, "items/seals")
 

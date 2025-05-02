bae124e19b729e90eea6aeaf5c6a01993d55e78d
Mills-44 <mylesbrodsky44@gmail.com>
2025-05-02 01:49:10 -0700

    New addons to game and corrected snacks to pool and booster

--- mills.lua
+++ mills.lua
@@ -21,31 +21,22 @@ MILLS.register_items(MILLS.PEPPER_JOKERS, "items/joker/pepper")
 MILLS.register_items(MILLS.SWEET_JOKERS, "items/joker/sweet")
 --MILLS.register_items(MILLS.SANDWICH_JOKERS, "items/joker/sandwich")
 
+-- Register custom ConsumableType: Snack
 SMODS.ConsumableType {
-  key = "Snack",
-  set = 'Snack',
-  primary_colour = MILLS.COLORS.COOKIE,
-  secondary_colour = MILLS.COLORS.COOKIE,
-  collection_rows = {3,3},
-  loc_txt ={
-      name = 'Snacks',
-      collection = 'Snacks', 
-  },
-  cards = {
-    ["c_mills_biscoff"] = true,
-    ["c_mills_blondwie"] = true,
-    ["c_mills_bubblegum"] = true,
-    ["c_mills_choffle"] = true,
-    ["c_mills_cream_puff"] = true,
-    ["c_mills_frosting"] = true,
-    ["c_mills_homeys_doney"] = true,
-    ["c_mills_kinder_egg"] = true,
-    ["c_mills_pretzel"] = true,
-    ["c_mills_rye_chip"] = true,
-    ["c_mills_slice_of_bread"] = true,
-    ["c_mills_whipping cream"] = true
-  }
+  object_type = "ConsumableType",
+  key = 'Snack',
+  default = 'c_mills_bubblegum',
+  collection_rows = { 3,4 },
+  primary_colour = HEX("572501"),
+  secondary_colour = HEX("D7A450"),
+  loc_txt = {
+      collection = 'Snack Cards',
+      name = 'Snack',
+      label = 'Snack',
+      },
+  shop_rate = 1.0,    
 }
+
 
 -- PATCH: Ensure pool is initialized before injection
 local orig_insert_pool = SMODS.insert_pool
@@ -60,12 +51,13 @@ MILLS.register_items(MILLS.SNACKBOOSTER, "items/snack/boosters")
 MILLS.register_items(MILLS.SNACKS, "items/snack/snacks")
 
 
-MILLS.register_items(MILLS.ENHANCEMENTS, "items/enhancements")
+MILLS.register_items(MILLS.SWEET_ENHANCEMENTS, "items/enhancements/sweet")
+MILLS.register_items(MILLS.ORE_ENHANCEMENTS, "items/enhancements/ore")
 
---MILLS.register_items(MILLS.SPECTRAL, "items/spectral")
+MILLS.register_items(MILLS.SPECTRAL, "items/spectral")
 
---MILLS.register_items(MILLS.SEALS, "items/seals")
+MILLS.register_items(MILLS.SEALS, "items/seals")
 
---MILLS.register_items(MILLS.TAGS, "items/tags")
+MILLS.register_items(MILLS.TAGS, "items/tags")
 
 --MILLS.register_items(MILLS.EDITIONS, "items/editions")

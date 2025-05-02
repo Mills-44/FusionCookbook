922c2e46168dbd5eed35c913ef2ad941f60b1499
Mills-44 <mylesbrodsky44@gmail.com>
2025-05-02 01:59:54 -0700

    Add files via upload

---
+++ items/enhancements/bou.lua
@@ -0,0 +1,29 @@
+SMODS.Enhancement {
+    key = "bou",
+    atlas = 'enhanc_fus', 
+    pos = {
+        x= 5,
+        y= 2
+    },
+    config = {
+        extra = {
+            chips = 50
+        }
+    },
+    any_rank = true,
+    no_suit = true,
+    replace_base_card = true,
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.chips
+        }}
+    end,
+    calculate = function(self, card, context)
+        if context.cardarea and context.main_scoring == G.play then
+            return {
+                chips = card.ability.extra.chips
+            }
+        end
+    end
+}

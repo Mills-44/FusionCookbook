209f21774cd3eb2532388c0a53700ba8031ad84d
Mills-44 <mylesbrodsky44@gmail.com>
2025-05-03 13:15:01 -0700

    Add files via upload

--- items/snack/kinder_egg.lua
+++ items/snack/kinder_egg.lua
@@ -11,27 +11,20 @@ SMODS.Consumable  {
     pools = { 
       Snack = true
    },
-    can_use = function(self)
-      return true
-    end,
+   can_use = function(self, card)
+    return #G.jokers.cards < G.jokers.config.card_limit or self.area == G.jokers
+  end,
     use = function(self, card, area)
       G.E_MANAGER:add_event(Event({
-        trigger = 'after', delay = 0.4,
+        trigger = 'after',
+        delay = 0.4,
         func = function()
-          local sweet_key = MILLS.random_choice_from_pool({Sweet = true})
-          if sweet_key and G.P_CENTERS[sweet_key] then
-            local j_card = SMODS.create_card {
-              type = 'Joker',
-              key = sweet_key,
-              area = G.jokers
-            }
-            j_card:add_to_deck()
-            G.jokers:emplace(j_card)
-            j_card:juice_up(0.3, 0.5)
-            play_sound('mills_nice')
-          else
-            print("❌ No valid Sweet Joker found, got:", tostring(sweet_key))
-          end
+          local chosen_joker = pseudorandom_element(MILLS.SWEET_JOKERS, pseudoseed('choice'))
+          play_sound('mills_nice')
+          local kinder = create_card("Joker", G.jokers, nil, nil, nil, nil, 'j_mills_'.. chosen_joker)    
+          kinder:add_to_deck()
+          G.jokers:emplace(kinder)
+          card:juice_up(0.3, 0.5)
           return true
         end
       }))

c24086f30808fdce3ebfb0d63c95639af9b34bd0
Rafa-beau <rafa.hana0807@gmail.com>
2025-05-04 02:15:56 -0300

    Add files via upload

---
+++ items/enhancements/fusion/power.lua
@@ -0,0 +1,25 @@
+SMODS.Enhancement {
+    key = "power",
+    atlas = 'enhanc_fus',
+    pos = {
+        x = 1,
+        y = 1
+    },
+    config = {extra = {
+         x_mult = 1.5
+        }},
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars={ 
+            card.ability.extra.x_mult 
+        }
+    }
+    end,
+    calculate = function(self, card, context)
+        if context.cardarea == G.play and context.main_scoring then
+           return {
+            x_mult = card.ability.extra.x_mult
+        }
+        end
+end
+}
---
+++ items/enhancements/fusion/sapph.lua
@@ -0,0 +1,27 @@
+SMODS.Enhancement {
+    key = "sapph",
+    atlas = 'enhanc_fus',
+    pos = {
+        x = 4, 
+        y = 0
+    },
+    config = {
+        extra = {
+            x_chips = 1.5
+        }
+    },
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars={
+                card.ability.extra.x_chips
+            }
+        }
+    end,
+    calculate = function(self, card, context)
+        if context.final_scoring_step and context.cardarea == G.hand then
+           return {
+            x_chips = card.ability.extra.x_chips
+        }
+        end
+end
+}

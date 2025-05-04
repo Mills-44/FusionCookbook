90a10840c38f1dfc94fd055bad74e8d0a65a72e5
Rafa-beau <rafa.hana0807@gmail.com>
2025-05-04 16:57:55 -0300

    Add files via upload

---
+++ items/enhancements/fusion/crack.lua
@@ -0,0 +1,24 @@
+SMODS.Enhancement {
+    key = "crack",
+    atlas = "enhanc_fus",
+    pos = {
+        x = 3,
+        y = 3
+    },
+    config = {extra = {x_mult = 4}},
+    loc_vars = function(self, info_queue, card)
+        return {vars={card.ability.extra.x_mult}}
+end,
+    calculate = function(self, card, context)
+        if context.cardarea == G.play and context.main_scoring then
+            return {
+                x_mult = card.ability.extra.x_mult
+            }
+        end
+        if context.cardarea == G.play and context.main_scoring then
+            return { 
+                remove = true 
+            }
+        end
+end
+}

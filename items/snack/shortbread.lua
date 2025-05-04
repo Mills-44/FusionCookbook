1fd416c7fb4af2cefccec320c1b0d7a1eccc86bb
Mills-44 <mylesbrodsky44@gmail.com>
2025-05-03 18:26:44 -0700

    Add files via upload

--- items/snack/shortbread.lua
+++ items/snack/shortbread.lua
@@ -24,44 +24,20 @@ SMODS.Consumable {
     use = function(self, card, area)
         local selected = G.hand.highlighted
         if #selected ~= 2 then return end
-
-        local base_rank = math.max(1, math.min(#MILLS.base_ranks, math.max(selected) - 1))
-        local result_rank = MILLS.base_ranks[base_rank]
-
-        for i = 1, #selected do
-            local percent = 0.85 + (i - 0.999) / (#selected - 0.998) * 0.3
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after', delay = 0.15, func = function()
-                    selected[i]:flip()
-                    selected[i]:juice_up(0.3, 0.3)
-                    return true
-                end
-            }))
+        
+        for i=1, #G.hand.highlighted do
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.1,
+            func = function()
+            local card = G.hand.highlighted[i]
+            card:flip()
+            card:juice_up(0.3, 0.5)
+            assert(SMODS.modify_rank(card, -1))
+            card:flip()
+            return true
         end
-
-        draw_card(G.hand, G.play, 1, 'up', false, selected[1], nil, true)
-        draw_card(G.hand, G.play, 1, 'up', false, selected[2], nil, true)
-
-        delay(0.4)
-        G.E_MANAGER:add_event(Event({
-            trigger = 'after', delay = 0.3, func = function()
-                selected[1]:destroy(true)
-                selected[2]:destroy(true)
-
-                local new_card = create_card(nil, G.hand, nil, nil, nil, nil, nil, result_rank)
-                new_card:add_to_deck()
-                G.hand:emplace(new_card)
-                return true
-            end
         }))
-
-        G.E_MANAGER:add_event(Event({
-            trigger = 'after', delay = 0.2, func = function()
-                G.hand:unhighlight_all()
-                return true
-            end
-        }))
-
-        delay(0.5)
     end
+end
 }

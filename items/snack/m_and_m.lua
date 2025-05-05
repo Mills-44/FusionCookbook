feafd5cc7c5b405149ed5dd5ce8431a22697a7aa
Mills-44 <mylesbrodsky44@gmail.com>
2025-05-04 21:06:43 -0700

    Add files via upload

--- items/snack/m_and_m.lua
+++ items/snack/m_and_m.lua
@@ -6,7 +6,7 @@ SMODS.Consumable {
     pos = { x = 0, y = 0 },
     config = {
         extra = {
-        odd = 3
+            odd = 3
         }
     },
     pools = {
@@ -15,28 +15,38 @@ SMODS.Consumable {
     cost = 4,
     unlocked = true,
     discovered = true,
+    consumable = true,
 
     loc_vars = function(self, info_queue, card)
-        return { vars = {
-            (G.GAME.probabilities.normal or 1), 
-            card.ability.extra.odd
+        return {
+            vars = {
+                (G.GAME.probabilities.normal or 1),
+                card.ability.extra.odd
+            }
         }
-    }
     end,
 
     can_use = function(self)
         return true
-      end,
+    end,
 
     use = function(self, card, area)
-        if pseudorandom('spectralodd') < G.GAME.probabilities.normal / card.ability.extra.odd then
-            return SMODS.create_card{
-            set = 'Spectral'
-            }
-        elseif pseudorandom('spectralodd') > G.GAME.probabilities.normal / card.ability.extra.odd then
-            return {
-            message = 'Nope!'
-            }
-        end
-    end
-}
+        G.E_MANAGER:add_event(Event({
+          trigger = 'after', 
+          delay = 0.4, 
+          func = function()
+          if G.consumeables.config.card_limit > #G.consumeables.cards and pseudorandom('spectralodd') < G.GAME.probabilities.normal / card.ability.extra.odd then
+            play_sound('mills_nice')
+            local card = create_card('Spectral', G.consumeables, nil, nil, nil, nil, nil, 'c_' )
+                      card:add_to_deck()
+                      G.consumeables:emplace(card)
+                      card:juice_up(0.3, 0.5)
+                else 
+                    return {
+                        message = 'Nope!'
+                    }
+                end
+                  return true end }))
+          delay(0.6)
+      end
+  }
